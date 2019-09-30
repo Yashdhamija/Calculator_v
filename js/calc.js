5ce366599aa88150c90af39ad8e523224936a3f6
Yashdhamija <yashdhamija4@gmail.com>
2019-09-30 18:06:35 -0400

    js

---
+++ js/calc.js
@@ -0,0 +1,54 @@
+let btns = document.getElementsByClassName("calc-btns");
+
+// const sin = document.getElementById("sin");
+// console.log(sin);
+// const sq_root = document.getElementById("sq-root");
+// console.log(sq_root);
+
+// vars for storing operands (number inputs)
+let input1 = '';
+let input2 = '';
+// flag whether theres a operation to perform
+let op_flag = false;
+// operator
+let operator = '';
+
+// function to take input
+// takes the input and appends it to the current display
+const getInput  = (ele) => {
+	const key = ele.target;
+	const action = key.dataset.action;
+	let display = document.getElementById("calcDisplay");
+	let currentVal = Number(display.innerHTML);
+
+	if(!action) {
+		let newDisplay = '';
+		let input = ele.target.innerHTML;
+
+		if(currentVal) 
+			newDisplay += currentVal;
+		newDisplay += input;
+
+		// if a operator is active i.e, 2nd input is being typed
+		if (operator) {
+			input2 = Number(newDisplay);
+		}
+		// else still first input
+		else {
+			input1 = Number(newDisplay);
+		}
+		display.innerHTML = newDisplay;
+	}
+	else {
+		if(action == "calculate") {
+			display.innerHTML = input1+input2;
+			return;
+		}
+		display.innerHTML = 0;
+	}
+}
+
+// adding event to btns
+for (var i = 0 ; i < btns.length; i++) {
+   btns[i].addEventListener("click", getInput, false ) ; 
+}
